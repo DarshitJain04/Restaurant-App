2d84f12acbba919aad8a46191e198e331a37f38a
Pratyaksh Tyagi <tyagipratyaksh@gmail.com>
2020-06-07 01:17:25 +0530

    Added LoginScreen UI

---
+++ App/Components/AuthScreenBox.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types'
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen'
+
+const AuthScreenBox = ({ children, title }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>{title}</Text>
+      <View style={styles.FirstBoxStyles}>{children}</View>
+    </View>
+  )
+}
+
+const ReusableStyles = {
+  backgroundColor: 'white',
+  height: hp('62%'),
+  width: wp('87%'),
+  borderRadius: 15,
+  alignSelf: 'center',
+}
+
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: hp('3.0%'),
+    color: 'white',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  FirstBoxStyles: {
+    ...ReusableStyles,
+  },
+  SecondBoxStyles: {
+    ...ReusableStyles,
+    width: 290,
+    height: 300,
+    opacity: 0.8,
+  },
+  ThirdBoxStyles: {
+    ...ReusableStyles,
+    width: 250,
+    height: 300,
+    opacity: 0.5,
+  },
+})
+
+AuthScreenBox.propTypes = {
+  children: PropTypes.array,
+  title: PropTypes.string,
+}
+
+//make this component available to the app
+export default AuthScreenBox
--- App/Screens/Auth/LoginScreen.js
+++ App/Screens/Auth/LoginScreen.js
@@ -1,20 +1,53 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import {
+  Text,
+  ImageBackground,
+  Dimensions,
+  StatusBar,
+  Image,
+  StyleSheet,
+} from 'react-native'
+import AuthScreenBox from '../../Components/AuthScreenBox'
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen'
 
-const LoginScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  ImageBackgroundStyles: {
+    width: width,
+    height: height,
+    resizeMode: 'cover',
+  },
+  heading: {
+    textAlign: 'center',
+    fontSize: hp('4.3%'),
+    marginTop: -25,
+  },
+  subheading: {
+    textAlign: 'center',
+    fontSize: hp('1.6%'),
   },
 })
 
-//make this component available to the app
+const LoginScreen = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <ImageBackground
+        source={require('../../Assets/Images/Background.png')}
+        style={styles.ImageBackgroundStyles}
+      >
+        <AuthScreenBox title="Sign In">
+          <Image source={require('../../Assets/Images/Plate.png')} />
+          <Text style={styles.heading}>Welcome Back</Text>
+          <Text style={styles.subheading}>We love to see you again.</Text>
+          <Text style={styles.subheading}>Form</Text>
+        </AuthScreenBox>
+      </ImageBackground>
+    </>
+  )
+}
 export default LoginScreen
