77a689451d039b0d487a56cf68a6b8bb9a15efbe
Pratyaksh Tyagi <tyagipratyaksh@gmail.com>
2020-06-08 03:07:35 +0530

    Avatar Added

--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import { Button } from 'react-native-elements'
+import { Button, Avatar } from 'react-native-elements'
 import * as firebase from 'firebase'
 import 'firebase/auth'
 
 const HomeScreen = ({ navigation }) => {
   const userData = navigation.getParam('user')
+  console.log(userData)
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
+      <Avatar
+        size="medium"
+        rounded
+        source={{
+          uri: userData.photoURL,
+        }}
+      />
+
       <Button
         title="Sign Out"
         onPress={() => {
