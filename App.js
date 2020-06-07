92825c47601229e8a9efbd9b311c407f8c356ef0
DarshitJain04 <jain.38@iitj.ac.in>
2020-06-07 17:58:24 +0530

    added additional fonts

--- App.js
+++ App.js
@@ -7,6 +7,16 @@ import { AppLoading } from 'expo'
 const loadFont = () => {
   return Font.loadAsync({
     'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-Black': require('./assets/fonts/Montserrat-Black.ttf'),
+    'Montserrat-BoldItalic': require('./assets/fonts/Montserrat-BoldItalic.ttf'),
+    'Montserrat-ExtraBold': require('./assets/fonts/Montserrat-ExtraBold.ttf'),
+    'Montserrat-ExtraLight': require('./assets/fonts/Montserrat-ExtraLight.ttf'),
+    'Montserrat-Italic': require('./assets/fonts/Montserrat-Italic.ttf'),
+    'Montserrat-Light': require('./assets/fonts/Montserrat-Light.ttf'),
+    'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-Thin': require('./assets/fonts/Montserrat-Thin.ttf'),
   })
 }
 
